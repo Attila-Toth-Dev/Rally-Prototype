4499a8a4fd709a7631e562835388ce8bac2e323a
Attila Toth <attilatwork@gmail.com>
2025-08-26 15:26:31 +1000

    UPDATED: working on refining wheel physics

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -1,6 +1,8 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[Serializable]
 enum Drivetrain
 {
     Rwd,
@@ -11,77 +13,117 @@ enum Drivetrain
 public class CarController : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private Wheel[] wheelColliders;
+    [SerializeField] private Wheel[] wheels;
 
     [Header("Engine")]
     [SerializeField] private float maxHorsepower;
+
+    [Header("Drivetrain")]
+    [SerializeField] private Drivetrain drivetrain = Drivetrain.Awd;
 
     [Header("Handling")]
     [SerializeField] private float maxSteerAngle;
     [SerializeField] private float steeringSensitivity;
 
+    [Header("Braking")]
+    [SerializeField] private float brakeForce;
+
     [Header("Controls")]
-    [SerializeField] private InputActionReference moveCarReference;
+    [SerializeField] private InputActionReference accelCarReference;
+    [SerializeField] private InputActionReference decelCarReference;
     [SerializeField] private InputActionReference steerCarReference;
+    [SerializeField] private InputActionReference handbrakeCarReference;
 
     [Header("Debugging")]
-    [SerializeField] private float moveAmount;
+    [SerializeField] private float accelAmount;
+    [SerializeField] private float decelAmount;
     [SerializeField] private float steerAmount;
-    [SerializeField] private float currentMove;
-    [SerializeField] private float currentSteer;
+    [SerializeField] private bool isBraking;
+    [SerializeField] private bool isHandbraking;
+
+    private float currentAccel;
+    private float currentDecel;
+    private float currentSteer;
 
     private void OnEnable()
     {
-        moveCarReference.action.Enable();
+        accelCarReference.action.Enable();
+        decelCarReference.action.Enable();
+
         steerCarReference.action.Enable();
+        handbrakeCarReference.action.Enable();
     }
 
     private void OnDisable()
     {
-        moveCarReference.action.Disable();
+        accelCarReference.action.Disable();
+        decelCarReference.action.Disable();
+
         steerCarReference.action.Disable();
+        handbrakeCarReference.action.Disable();
     }
 
     private void Update()
     {
+        GetInputs();
+
         CalculateMovement();
         CalculateSteering();
     }
 
+    private void GetInputs()
+    {
+        accelAmount = accelCarReference.action.ReadValue<float>();
+        decelAmount = decelCarReference.action.ReadValue<float>();
+        steerAmount = steerCarReference.action.ReadValue<float>();
+
+        isBraking = decelCarReference.action.IsInProgress();
+        isHandbraking = handbrakeCarReference.action.IsInProgress();
+    }
+
     private void CalculateMovement()
     {
-        moveAmount = moveCarReference.action.ReadValue<float>();
-
-
+        currentAccel = Mathf.Lerp(currentAccel, maxHorsepower * accelAmount, Time.deltaTime);
+        currentDecel = Mathf.Lerp(currentDecel, brakeForce * decelAmount, Time.deltaTime);
     }
 
     private void CalculateSteering()
     {
-        steerAmount = steerCarReference.action.ReadValue<float>();
-
-        currentSteer = Mathf.Lerp(currentSteer, maxSteerAngle, steeringSensitivity);
+        currentSteer = Mathf.Lerp(currentSteer, maxSteerAngle * steerAmount, steeringSensitivity * Time.deltaTime);
     }
 
     private void FixedUpdate()
     {
-        MoveCar();
+        Moving();
+        Braking();
+
         SteerCar();
+        Handbrake();
     }
 
-    private void MoveCar()
+    private void Moving()
     {
-        foreach (Wheel wheel in wheelColliders)
-        {
-            wheel.WheelCollider.motorTorque = 1000 * moveAmount;
-        }
+        foreach (Wheel wheel in wheels)
+            wheel.WheelCollider.motorTorque = currentAccel;
+    }
+
+    private void Braking()
+    {
+        foreach (Wheel wheel in wheels)
+            wheel.WheelCollider.brakeTorque = isBraking ? brakeForce * decelAmount : 0;
+    }
+
+    private void Handbrake()
+    {
+        foreach (Wheel wheel in wheels)
+            if (wheel.Axle == Axle.Rear)
+                wheel.WheelCollider.brakeTorque = isHandbraking ? 10000 : 0;
     }
 
     private void SteerCar()
     {
-        foreach (Wheel wheel in wheelColliders)
-        {
-            if(wheel.Axle == Axle.Front)
-                wheel.WheelCollider.steerAngle = 100 * steerAmount;
-        }
+        foreach (Wheel wheel in wheels)
+            if (wheel.Axle == Axle.Front)
+                wheel.WheelCollider.steerAngle = currentSteer;
     }
 }
